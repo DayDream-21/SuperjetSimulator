37f5172d6945e676fd16722bc23cbf0cc7300d75
DayDream-21 <vova13158@gmail.com>
2022-11-24 18:47:53 +0300

    Initial commit

---
+++ src/main/java/com/slavamashkov/superjetsimulator/SuperjetSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.slavamashkov.superjetsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperjetSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperjetSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slavamashkov/superjetsimulator/SuperjetSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slavamashkov.superjetsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperjetSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
