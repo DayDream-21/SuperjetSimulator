2d8999238a3ee5b01dcb33502303e9aba9ee4e84
DayDream-21 <vova13158@gmail.com>
2023-01-11 19:13:50 +0300

    Upgrade malfunction logic  * Now the load reduction logic is moved in the ElecGenLeft(Right)Overload    class  * Now when the consumers are switched off, the maximum load of the    engine decreases to the normal value

--- src/main/java/com/slavamashkov/superjetsimulator/malfunctions/ElecGenRightOverload.java
+++ src/main/java/com/slavamashkov/superjetsimulator/malfunctions/ElecGenRightOverload.java
@@ -2,13 +2,17 @@ package com.slavamashkov.superjetsimulator.malfunctions;
 
 import com.slavamashkov.superjetsimulator.controllers.SelectionPanelController;
 import com.slavamashkov.superjetsimulator.controllers.bottom_layer.ElecUnitsConnectionsController;
+import io.reactivex.Observable;
+import io.reactivex.rxjavafx.observables.JavaFxObservable;
 import javafx.application.Platform;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @RequiredArgsConstructor
@@ -18,20 +22,45 @@ public class ElecGenRightOverload extends Malfunction {
 
     @Override
     public void executeMalfunction() {
+        Observable<MouseEvent> observableGalley = JavaFxObservable.eventsOf(
+                selectionPanelController.getGalleyButton(), MouseEvent.MOUSE_CLICKED
+        );
+
+        Observable<MouseEvent> observableMain = JavaFxObservable.eventsOf(
+                selectionPanelController.getFuelRightMAINButton(), MouseEvent.MOUSE_CLICKED
+        );
+
         ExecutorService executor = Executors.newFixedThreadPool(1);
         Label loadValueLabel = elecUnitsConnectionsController.getRightDrivePercentValue();
 
+        observableGalley.subscribe(a -> dropValue(loadValueLabel, 20));
+
+        observableMain.subscribe(a -> dropValue(loadValueLabel, 40));
+
         Runnable task = () -> {
             while (true) {
+                final int maxLoad = 130;
+                AtomicInteger newLoad = new AtomicInteger(100);
                 if (elecUnitsConnectionsController.isRightEngineConnected()) {
                     Platform.runLater(() -> {
-                        int maxLoad = 110;
-
                         if (selectionPanelController.isGalleyButtonSwitched()) {
-                            maxLoad = 100;
+                            newLoad.set(maxLoad - 10);
+
+                            if (selectionPanelController.isFuelRightADDTButtonSwitched() && selectionPanelController.isFuelRightMAINButtonSwitched()) {
+                                newLoad.set(maxLoad - 30);
+                            } else {
+                                newLoad.set(maxLoad - 10);
+                            }
+                        } else {
+                            if (selectionPanelController.isFuelRightADDTButtonSwitched() && selectionPanelController.isFuelRightMAINButtonSwitched()) {
+                                newLoad.set(maxLoad - 20);
+                            } else {
+                                newLoad.set(maxLoad);
+                            }
+                            newLoad.set(maxLoad);
                         }
 
-                        if (Integer.parseInt(loadValueLabel.getText()) < maxLoad) {
+                        if (Integer.parseInt(loadValueLabel.getText()) < newLoad.get()) {
                             int prevValue = Integer.parseInt(loadValueLabel.getText());
                             int nextValue = prevValue + 1;
                             loadValueLabel.setText(String.valueOf(nextValue));
@@ -56,5 +85,16 @@ public class ElecGenRightOverload extends Malfunction {
     @Override
     public String toString() {
         return "ELEC GEN R OVERLOAD";
+    }
+
+    private void dropValue(Label label, int value) {
+        int prevValue = Integer.parseInt(label.getText());
+
+        if (prevValue < 90) {
+            return;
+        }
+
+        int nextValue = prevValue - value;
+        label.setText(String.valueOf(nextValue));
     }
 }
